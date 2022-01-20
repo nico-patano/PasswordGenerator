31a8e2287fc66eb81e9d300f55e76fd47b328151
Nicholas Patano <nicholaspatano@gmail.com>
2022-01-19 20:04:16 -0500

    syntax adjustments

--- script.js
+++ script.js
@@ -59,8 +59,9 @@ function generatePassword() {
   let lowerCase= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   let number= ["0","1","2","3","4","5","6","7","8","9"];
   let symbol= ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
-        
 
+  
+//options to choose from character types
   if(includeLower===true){
     selectedItems=selectedItems.concat(lowerCase);
   } 
@@ -76,7 +77,7 @@ function generatePassword() {
       selectedItems=selectedItems.concat(symbol);
   }
 
-      
+   //final passcode   
   let finalPassword = [];
   
   for (let i=0; i < (length.valueOf()); i++){
